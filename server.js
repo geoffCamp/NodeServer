d735dad4dee73127a51e0e4a8a758b0ccaa392a7
Geofferson <Geofferson@Geoffersons-MacBook-Pro.local>
2016-09-04 19:59:09 -0400

    connecting with mongo

--- server.js
+++ server.js
@@ -1,8 +1,11 @@
-const http = require('http');
+const http = require('http'),
+      express = require('express');
+      path = require('path');
 
 const hostname = '127.0.0.1';
 const port = 3000;
 
+/*
 const server = http.createServer((req, res) => {
     res.statusCode = 200;
     res.setHeader('Content-Type', 'text/plain');
@@ -11,4 +14,15 @@ const server = http.createServer((req, res) => {
 
 server.listen(port, hostname, () => {
     console.log("Server running at http://"+hostname+":"+port+"/");
+});
+*/
+
+var app = express();
+app.set('port', process.env.PORT || port);
+app.get('/', function(req, res) {
+    res.send('<html><body><h1>HelloWorld</h1></body></html>');
+});
+
+http.createServer(app).listen(app.get('port'), function() {
+    console.log('Express server listening on port ' + app.get('port'));
 });
